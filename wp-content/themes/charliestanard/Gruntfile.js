1f3d39527644dc1df3ff63c61889ccd8ee43192c
Charlie Stanard <louis.stanard@gmail.com>
2016-01-12 21:15:34 -0700

    Update Grunt workflow

--- wp-content/themes/charliestanard/Gruntfile.js
+++ wp-content/themes/charliestanard/Gruntfile.js
@@ -4,9 +4,72 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 
 		// Scripts
+
 		// Styles
+		sass: {
+			options: { // https://github.com/sass/node-sass#options
+				sourceMap: true,
+				style: 'expanded',
+				indentType: 'tab',
+				indentWidth: 1
+				// sourceComments: true,
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'sass',
+					src: ['*.scss'],
+					dest: 'css',
+					ext: '.css'
+				}]
+			}
+		},
+
+		postcss: {
+			options: {
+				map: {
+					inline: false,
+					annotation: 'css/'
+				},
+				processors: [
+					require('pixrem')(),
+					require('autoprefixer')({
+						browsers: 'last 4 versions'
+					}),
+					require('postcss-cssnext')(),
+				]
+			},
+			dist: {
+				src: 'css/*.css' // can also use files: {'dest': ['src']}
+			}
+		},
+
+		cssnano: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'css/site.min.css': ['css/site.css']
+				}
+			}
+		},
+
 		// Images
+
 		// Misc
+		watch: {
+			grunt: {
+				files: ['Gruntfile.js']
+			},
+			styles: {
+				files: ['sass/*.scss', 'sass/**/*.scss'],
+				tasks: ['style'],
+				options: {
+					// livereload: true
+				}
+			}
+		}
 
 	});
 
@@ -14,12 +77,15 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-compress');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-newer');
 	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-cssnano');
+	grunt.loadNpmTasks('grunt-newer');
+	grunt.loadNpmTasks('grunt-sass');
 
 	// Tasks
+	grunt.registerTask('default', ['sass', 'postcss', 'cssnano']);
+	grunt.registerTask('style', ['sass', 'postcss', 'cssnano']);
 
 };
